87a6429659544a8ab28c890186c90490cf51385e
Tu276 <tu276nathan@gmail.com>
2023-03-15 15:16:06 +0300

    102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,4 +1,44 @@
-#this script 
-#this script 
-#this script
-#this script
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+int main(void)
+{
+    pid_t pid;
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        pid = fork();
+
+        if (pid == -1)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0)
+        {
+            // Child process
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            // Parent process
+            printf("Zombie process created, PID: %d\n", pid);
+        }
+    }
+
+    infinite_while();
+
+    return (0);
+}
